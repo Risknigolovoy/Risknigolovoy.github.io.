40e16f723737d98bc54cd4a7b1fe79df4add16eb
Risknigolovoy <risknigolovoy.@gmail.com>
2025-06-22 01:24:09 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-function nextScene() {
-  document.getElementById('scene').innerHTML = `
-    <h2>Соул рядом 🐾</h2>
-    <p>Он чувствует, что ты здесь. Он будет вести тебя по миру воспоминаний.</p>
-    <audio controls src="audio/scene1.m4a"></audio>
-  `;
-}
